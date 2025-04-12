590b3e434db83754ad15911ad5bc264d17fe0daa
Bernardo Ramos Alonso Ribeiro <quenity@outlook.com>
2025-04-12 17:46:53 -0300

    refactor: Atualizando a main com a função floyd separada

--- src/main.py
+++ src/main.py
@@ -1,54 +1,7 @@
 from visualization import draw_graph
+from floyd import floyd_warshall_with_paths, reconstruct_path
 
 INF = float('inf')
-
-def floyd_warshall_with_paths(adj):
-    """
-    Executa o algoritmo de Floyd-Warshall e retorna:
-    - a matriz de distâncias mínimas
-    - uma matriz com os caminhos reconstruídos
-    """
-    n = len(adj)
-    
-    # Inicializa distâncias e caminhos
-    distance = [[INF] * n for _ in range(n)]
-    next_node = [[None] * n for _ in range(n)]
-
-    # Inicialização com base na matriz de adjacência
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                distance[i][j] = 0
-                next_node[i][j] = i
-            elif adj[i][j] != INF:
-                distance[i][j] = adj[i][j]
-                next_node[i][j] = j
-
-    # Algoritmo de Floyd-Warshall com reconstrução de caminho
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if distance[i][k] + distance[k][j] < distance[i][j]:
-                    distance[i][j] = distance[i][k] + distance[k][j]
-                    next_node[i][j] = next_node[i][k]
-
-    return distance, next_node
-
-def reconstruct_path(u, v, next_node):
-    """
-    Reconstrói o caminho de u até v usando a matriz next_node.
-    """
-    if next_node[u][v] is None:
-        return []
-    path = [u]
-    while u != v:
-        u = next_node[u][v]
-        path.append(u)
-    return path
-
-
-
-
 adj = [
     [0,   3, INF, 5],
     [2,   0, INF, 4],
