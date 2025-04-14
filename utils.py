2ffd77220f757fd62e50b89183c56a76a69330ca
Bernardo Ramos Alonso Ribeiro <quenity@outlook.com>
2025-04-13 22:19:56 -0300

    feat: Adicionando uma utils com matrizes caso queria testar sem ser aleatório

---
+++ utils.py
@@ -0,0 +1,33 @@
+INF = float('inf')
+
+# Matriz 2x2
+adj_2 = [
+    [0,     1],
+    [INF,   0]
+]
+
+# Matriz 3x3
+adj_3 = [
+    [0,     4,     INF],
+    [INF,   0,     2],
+    [1,     INF,   0]
+]
+
+# Matriz 5x5
+adj_5 = [
+    [0,     3,     8,     INF,  -4],
+    [INF,   0,     INF,   1,     7],
+    [INF,   4,     0,     INF,  INF],
+    [2,     INF,  -5,     0,     INF],
+    [INF,   INF,   INF,   6,     0]
+]
+
+# Matriz 6x6
+adj_6 = [
+    [0,     5,     INF,   10,    INF,   INF],
+    [INF,   0,     3,     INF,   INF,   9],
+    [INF,   INF,   0,     1,     INF,   INF],
+    [INF,   INF,   INF,   0,     2,     INF],
+    [INF,   INF,   INF,   INF,   0,     1],
+    [INF,   INF,   INF,   INF,   INF,   0]
+]
