9a762434b91d6bb2c65d8f872b79a0b8e2b91adf
Bernardo Ramos Alonso Ribeiro <quenity@outlook.com>
2025-04-13 22:08:15 -0300

    remove: tirando o conteudo da main aleatória(foi refatorada para outro arquivo)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from visualization import draw_graph
 from floyd import floyd_warshall_with_paths, reconstruct_path
-from geradorMatriz import gerar_matriz_adj
+
 
 INF = float('inf')
 adj = [
@@ -15,28 +15,12 @@ dist, next_node = floyd_warshall_with_paths(adj)
 
 
 # Exemplo: caminho mínimo de 0 para 2
-path_0_2 = reconstruct_path(0, 2, next_node)
+path_0_2 = reconstruct_path(0, 2, next_node,adj)
 print("Caminho mínimo de 0 para 3:", path_0_2)
 print("Distância mínima:", dist[0][2])
 draw_graph(adj, path_0_2)
 
+# Teste com matriz gerada aleatoriamente
 
-blele=gerar_matriz_adj(5, 10, 5)
-
-dist1, next_node1 = floyd_warshall_with_paths(blele)
-
-path_0_3= reconstruct_path(0, 3, next_node1)
-print("Caminho mínimo de 0 para 5:", path_0_3)
-print("Distância mínima:", dist1[0][3])
-# Visualizar grafo e o caminho mínimo de 0 até 2
-
-for row in blele:
-    print(row)
-print("---"*20)
-for row in dist1:
-    print(row)
-
-
-draw_graph(blele,path_0_3)
 
 
