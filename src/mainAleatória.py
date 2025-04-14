07b92e343a304d388ad0d404155013051c7f9deb
Bernardo Ramos Alonso Ribeiro <quenity@outlook.com>
2025-04-13 22:37:29 -0300

    docs: Adicionando comentarios a mainaleatória

--- src/mainAleatória.py
+++ src/mainAleatória.py
@@ -2,7 +2,8 @@ from visualization import draw_graph
 from floyd import floyd_warshall_with_paths, reconstruct_path
 from geradorMatriz import gerar_matriz_adj
 
-blele=gerar_matriz_adj(5, 10, 5)
+##n_pontos,n_conexoes ,peso_max respectivamente
+blele=gerar_matriz_adj(5, 5, 5) # Gerar matriz gera uma arvore geradora minima caso o numero de conexões seja menor que n-1
 
 
 print("Matriz de adjacência gerada aleatoriamente:")
@@ -11,8 +12,9 @@ print("---"*20)
 dist1, next_node1 = floyd_warshall_with_paths(blele)
 
 path_0_3= reconstruct_path(0, 3, next_node1,blele)
-print("Caminho mínimo de 0 para 5:", path_0_3)
+print("Caminho mínimo de 0 para 3:", path_0_3)
 print("Distância mínima:", dist1[0][3])
+print("---"*20)
 # Visualizar grafo e o caminho mínimo de 0 até 2
 
 for row in blele:
